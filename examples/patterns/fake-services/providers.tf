294cca03c32d167691ea3bc507bf32e7ae32e8f6
Tony Phan <tphan@hashicorp.com>
2023-07-04 20:00:06 +1000

    feat: Add fake-service kubernetes

--- examples/patterns/fake-services/providers.tf
+++ examples/patterns/fake-services/providers.tf
@@ -14,3 +14,17 @@ terraform {
 provider "aws" {
   region = data.terraform_remote_state.tcm.outputs.aws_region
 }
+
+data "aws_eks_cluster" "default" {
+  name = data.terraform_remote_state.tcm.outputs.deployment_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.name]
+    command     = "aws"
+  }
+}
